d3f114eb75aaf9b208646ee1a4ed28a175582c21
chris ellsmore <ellsmore@chriss-MacBook-Pro.local>
2016-07-26 20:41:23 +1000

    Created Backbone AppView.js

---
+++ app/assets/javascripts/views/AppView.js
@@ -0,0 +1,16 @@
+var app = app || {};
+
+app.AppView = Backbone.View.extend({
+    el: '#main', //This div has not yet been created
+
+    render: function() {
+        var content = $('#appview').html();
+
+        this.$el.html(content);
+
+        this.collection.each(function(flight) {
+            var flightListView = new app.FlightListView({ model: flight });
+            flightListView.render();
+        });
+    }
+});
