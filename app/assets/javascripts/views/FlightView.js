3af2ab72a440b3bad486ff329bec3981f3f50e73
Tripti <Tripti@192-168-1-4.tpgi.com.au>
2016-07-27 22:51:35 +1000

    updated plane seating plan and seat selection view

--- app/assets/javascripts/views/FlightView.js
+++ app/assets/javascripts/views/FlightView.js
@@ -2,11 +2,22 @@ var app = app|| {};
 
 app.FlightView = Backbone.View.extend({
   el: '#main',
+  events: {
+    'click td.planeSeats': 'seatSelection',
+    'click button#confirmSeat': 'makeReservation'
+  },
   render: function(){
     var flightViewTemplate = $('#flightView').html();
     console.log(flightViewTemplate);
     var flightViewTemplater = _.template(flightViewTemplate);
 
     this.$el.html( flightViewTemplater( this.model.toJSON() ) );
+  },
+  seatSelection: function(){
+    var seatId = $('td').attr(id);
+    console.log(seatId);
+  },
+  makeReservation: function(){
+
   }
 });
